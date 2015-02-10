3cddbda833cd572004d25f47f43b355e3254f5e4
c-brenn <brennc27@tcd.ie>
2015-02-10 18:28:34 +0000

    initial commit, really basic outline of site created

---
+++ node/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+var port = 3000;
+app.listen(port);
+
+app.get('/', function(req, res){
+	res.render('index', {});
+});
+
+app.get('/results', function(req, res) {
+	res.render('results', {});
+});
+
+app.get('/login', function(req, res) {
+	res.render('login', {});
+});
