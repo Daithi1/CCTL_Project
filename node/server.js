b9b7417ecf75af90b32c897356f39b121713c2ba
c-brenn <brennc27@tcd.ie>
2015-02-11 22:11:39 +0000

    changed port to 80 (internet port) to run on server

--- node/server.js
+++ node/server.js
@@ -1,13 +1,13 @@
 var express  = require('express');
-var app      = express();
-var port     = 3000;
+var app = express();
+var port = 80;
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
-var morgan       = require('morgan');
+var flash = require('connect-flash');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var configDB = require('./config/database.js');
 
